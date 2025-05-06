7482ec7bbe4ffc901d9991b526f736fc036c9ca8
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-05-06 23:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener("click", function (e) {
+  e.preventDefault(); // Prevent form reload
+
+  let title = document.getElementById("title").value.trim();
+  let author = document.getElementById("author").value.trim();
+  let isbn = document.getElementById("isbn").value.trim();
+
+  if (!title || !author || !isbn) {
+    alert("Please fill all fields.");
+    return;
+  }
+	
+	let table = document.getElementById("book-list");
+
+	let newRow = document.createElement("tr");
+
+	newRow.innerHTML = `
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">Clear</button></td>
+	`;
+	  table.appendChild(newRow);
+
+	document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+});
+
+document.getElementById("book-list").addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    let row = e.target.closest("tr");
+    row.remove();
+  }
+});
